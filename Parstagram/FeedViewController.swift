823356cd22cb85867600e1836dba8c56f5f71a3f
Regina Bass <reginabassvdl@gmail.com>
2019-11-20 01:58:43 -0500

    Comments section added

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -13,7 +13,8 @@ import Alamofire
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-
+    //let myRefreshControl = UIRefreshControl()
+    
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,7 +22,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         let query = PFQuery(className:"Posts")
-        query.includeKey("author")   //go and fetch the actual object
+        query.includeKeys(["author", "comments", "comments.author"])   //go and fetch the actual object
         query.limit = 20
         
         DataRequest.addAcceptableImageContentTypes(["application/octet-stream"])
@@ -39,29 +40,71 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        self.tableView.rowHeight = 300;
-
-        // Do any additional setup after loading the view.
+        
+        //myRefreshControl.addTarget(self, action: #selector(getter: tableView), for: .valueChanged)
+        //tableView.refreshControl = myRefreshControl
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
         return posts.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell  //allows you to configure outlets
+        
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell  //allows you to configure outlets
+            
+            
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            
+            
+            cell.captionLabel.text = post["caption"] as! String
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            cell.photoView.af_setImage(withURL: url)
+            
+            //self.myRefreshControl.endRefreshing()
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            return cell
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let post = posts[indexPath.row]
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "this is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
         
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
-        
-        
-        cell.captionLabel.text = post["caption"] as! String
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        cell.photoView.af_setImage(withURL: url)
-        
-        return cell
+        post.add(comment, forKey: "comments")
+        post.saveInBackground { (success, error) in
+            if success{
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+                
+            }
+        }
     }
 
     /*
@@ -87,5 +130,5 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
     
-
+    
 }
